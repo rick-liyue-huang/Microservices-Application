62f9d0f0455a833061db39e952ce38be70fd9b5e
Rick Huang <RickHuang@rauland.com.au>
2025-05-30 23:39:29 +1000

    ci: initiate the userService

---
+++ eCormerceSolution.UsersService/eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        // todo: add services to the Ioc container
+        // Core services often include data access, caching and other low level component
+
+        return services;
+    }
+}
---
+++ eCormerceSolution.UsersService/eCommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        // todo: add services to the Ioc container
+        // infrastructure services often include data access, caching and other low level component
+
+        return services;
+    }
+}
